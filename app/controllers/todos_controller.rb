ada1714f05272667d421aaf81b8f69cd7d599fbd
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-01-05 20:54:05 +0530

    Todos to user mapping

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,47 +1,53 @@
 class TodosController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_todo, only: [:edit, :update, :destroy]
 
-    def index
-        @todos=Todo.all
+  def index
+    @todos = current_user.todos
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params)
+
+    if @todo.save
+      redirect_to todos_path, notice: "Todo was successfully created."
+    else
+      flash.now[:alert] = "Failed to create todo."
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @todo=Todo.new
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path, notice: "Todo was successfully updated."
+    else
+      flash.now[:alert] = "Failed to update todo."
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @todo=Todo.new(todo_params)
-        if @todo.save
-            redirect_to todos_path, notice: "Todo was successfully created."
-        else
-            flash.now[:alert] = "Failed to create todo."
-            render :new, status: :unprocessable_entity
-        end
+  def destroy
+    @todo.destroy
+    redirect_to todos_path, notice: "Todo was successfully deleted."
+  end
 
-    end
+  private
 
-    def edit
-        @todo=Todo.find_by(id: params[:id])
-    end
+  # ✅ only current_user’s todos can be accessed
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to todos_path, alert: "You are not authorized to access that todo."
+  end
 
-    def update
-        @todo=Todo.find_by(id: params[:id])
-        if @todo.update(todo_params)
-            redirect_to todos_path, notice: "Todo was successfully updated."
-        else
-            flash.now[:alert] = "Failed to update todo."
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @todo=Todo.find_by(id: params[:id])
-        @todo.destroy
-        redirect_to todos_path, notice: "Todo was successfully deleted."
-    end
-
-    private
-    def todo_params
-        params.require(:todo).permit(:title, :completed)
-    end
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
+  end
 end
